ba25ce490ef794f48d73c7ae6fa7e98fb4d2a70f
AnnisaFitry <2041720123@student.polinema.ac.id>
2023-02-21 07:18:54 +0700

    modul_1

---
+++ week1/praktikum/template.js
@@ -0,0 +1,6 @@
+let fname = 'Annisa';
+let lname = 'Fitri Yuliandra';
+let age = prompt("Masukkan umur Annisa Fitri Yuliandra!");
+
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
